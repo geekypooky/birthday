4843edac5e1d067187b6f4b493e7b00f08e03781
Pooji <pooji@gmail.com>
2025-12-25 11:34:36 +0530

    Add all current project changes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: './', // ensures relative paths in build
   plugins: [react()],
   build: {
-    outDir: 'docs',
+    outDir: 'docs', // Vercel will serve from this folder
     emptyOutDir: true,
   },
 })
